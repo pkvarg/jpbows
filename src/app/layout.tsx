762378fd9c96c160c13e81f61c22303b905ed9eb
peter varga <peter.varga@staffino.com>
2025-08-26 21:48:39 +0200

    build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 //import { Geist, Geist_Mono } from 'next/font/google'
-import { Montserrat, Montserrat_Alternates } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import './globals.css'
 import { NextIntlClientProvider } from 'next-intl'
 import NavbarServer from './components/translationServerComponents/NavbarServer'
