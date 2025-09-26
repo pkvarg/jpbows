1df5f24ce3358c0c86aba3ec9aa3dfdba39fbe45
peter varga <peter.varga@staffino.com>
2025-09-26 07:35:39 +0200

    build

--- src/app/[locale]/about-me/page.tsx
+++ src/app/[locale]/about-me/page.tsx
@@ -1,22 +1,22 @@
 'use client'
 import React from 'react'
-import { motion } from 'framer-motion'
+//import { motion } from 'framer-motion'
 import About_Me from '@/app/components/AboutMe'
 
 const AboutMe = () => {
-  const fadeInUp = {
-    initial: { opacity: 0, y: 60 },
-    animate: { opacity: 1, y: 0 },
-    transition: { duration: 0.8, ease: 'easeOut' },
-  }
+  // const fadeInUp = {
+  //   initial: { opacity: 0, y: 60 },
+  //   animate: { opacity: 1, y: 0 },
+  //   transition: { duration: 0.8, ease: 'easeOut' },
+  // }
 
-  const staggerContainer = {
-    animate: {
-      transition: {
-        staggerChildren: 0.3,
-      },
-    },
-  }
+  // const staggerContainer = {
+  //   animate: {
+  //     transition: {
+  //       staggerChildren: 0.3,
+  //     },
+  //   },
+  // }
 
   return (
     <div className="relative min-h-screen bg-[#fefefe]">
