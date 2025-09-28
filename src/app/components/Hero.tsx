5d2254df588db4e4002506aba090966a757e4c29
peter varga <peter.varga@staffino.com>
2025-09-28 21:25:51 +0200

    build

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import { Link } from '@/i18n/routing'
+//import { Link } from '@/i18n/routing'
 
 const Hero = () => {
   return (
